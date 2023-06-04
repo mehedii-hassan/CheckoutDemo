d3b66327465b9957dd8904ad58a432b471b2c2d3
Mehedi Hassan <mehedi.github@gmail.com>
2023-06-04 23:53:37 +0600

    updated

--- app/src/main/java/com/example/checkout/ui/fragments/BranchCheckoutFragment.kt
+++ app/src/main/java/com/example/checkout/ui/fragments/BranchCheckoutFragment.kt
@@ -13,7 +13,7 @@ class BranchCheckoutFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_branch_checkout, container, false)
     }
